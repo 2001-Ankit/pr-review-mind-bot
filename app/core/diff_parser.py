7cc15868068b2e02b751582781e03581f15c3624
Ankit Rai <raia85859@gmail.com>
2026-03-02 18:54:14 +0545

    feat: handling deleted and binary files

--- app/core/diff_parser.py
+++ app/core/diff_parser.py
@@ -5,6 +5,7 @@ from typing import List
 @dataclass
 class Hunk:
     header: str
+    raw_lines: List[str]
     added_lines: List[str]
     removed_lines: List[str]
     context_lines: List[str]
@@ -16,26 +17,27 @@ class FileChange:
     hunks: List[Hunk]
     total_added: int
     total_removed: int
+    is_deleted:bool = False
+    is_new:bool = False
 
 
 class DiffParser:
     def parse(self, diff: str) -> List[FileChange]:
         lines = diff.splitlines()
-        files: List[FileChange] = []
+        files = []
 
         current_file = None
         current_hunk = None
 
         for line in lines:
-            
-            # New file section
+
             if line.startswith("diff --git"):
                 if current_file:
                     files.append(current_file)
 
-                parts = line.split(" ")
-                file_path = parts[-1]  # usually b/path/to/file.py
-                file_name = file_path.replace("b/", "")
+                parts = line.split()
+                b_path = parts[3] if len(parts) >= 4 else ""
+                file_name = b_path[2:] if b_path.startswith("b/") else b_path
 
                 current_file = FileChange(
                     file_name=file_name,
@@ -45,18 +47,24 @@ class DiffParser:
                 )
                 current_hunk = None
 
-            # New hunk
+            elif current_file and line.startswith("new file mode"):
+                current_file.is_new = True
+
+            elif current_file and line.startswith("deleted file mode"):
+                current_file.is_deleted = True
+
             elif line.startswith("@@") and current_file:
                 current_hunk = Hunk(
                     header=line,
                     added_lines=[],
                     removed_lines=[],
                     context_lines=[],
+                    raw_lines=[],
                 )
                 current_file.hunks.append(current_hunk)
 
-            # Inside a hunk
             elif current_hunk:
+                current_hunk.raw_lines.append(line)
 
                 if line.startswith("+") and not line.startswith("+++"):
                     current_hunk.added_lines.append(line[1:])
@@ -69,12 +77,10 @@ class DiffParser:
                 elif line.startswith(" "):
                     current_hunk.context_lines.append(line[1:])
 
-        # Append last file
         if current_file:
             files.append(current_file)
 
         return files
-
 
 if __name__ == "__main__":
     parser = DiffParser()
